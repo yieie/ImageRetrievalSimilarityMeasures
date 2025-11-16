edf34aaab03a8b7ed8f67a1d1e50c9200665face
Jaxx <envy-crease-thee@duck.com>
2025-11-16 20:56:56 +0800

    處理原data檔無用column

---
+++ Remove_Unuseful_Column.py
@@ -0,0 +1,41 @@
+import os
+"""
+This script removes unuseful columns from raw data
+"""
+
+
+input_folder = r"raw_data"
+output_folder = r"processed_data"
+
+os.makedirs(output_folder, exist_ok=True)
+
+for filename in os.listdir(input_folder):
+    if filename.lower().endswith(".txt"):
+        input_path = os.path.join(input_folder, filename)
+        output_path = os.path.join(output_folder, filename)
+
+        with open(input_path, "r", encoding="utf-8") as f:
+            # remove first and last row
+            lines = f.readlines()[1:-1]
+
+        processed_lines = []
+
+        for line in lines:
+            cols = line.strip().split()
+
+            # remove scalable's col
+            for idx in range(335, 79, -1):
+                if idx < len(cols):
+                    cols.pop(idx)
+
+            # remove path and counter
+            if len(cols) >= 4:
+                cols.pop(-2)
+                cols.pop(-3)
+
+            processed_lines.append(" ".join(cols) + "\n")
+
+        with open(output_path, "w", encoding="utf-8") as f:
+            f.writelines(processed_lines)
+
+print("all done")
